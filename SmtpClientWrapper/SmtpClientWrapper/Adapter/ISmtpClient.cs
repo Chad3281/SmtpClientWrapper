51d2c1fcdd2f53342ac609a0334a6fc281d74c8f
Chad3281 <chad3281@gmail.com>
2017-06-30 23:48:41 -0400

    Add files via upload

---
+++ SmtpClientWrapper/SmtpClientWrapper/Adapter/ISmtpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace SmtpClientWrapper.Adapter
+{
+    // Target of Adapter pattern:
+    public interface ISmtpClient
+    {
+        string Host { get; set; }
+
+        int Port { get; set; }
+        int Timeout { get; set; }
+
+        bool UseDefaultCredentials { get; set; }
+        bool EnableSsl { get; set; }
+
+        void Send(MailMessage message);
+        void Send(string from, string to, string subject, string body);
+    }
+}
---
+++ SmtpClientWrapper/SmtpClientWrapper/Adapter/SmtpClientAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmtpClientWrapper.Adapter
+{
+    // Adapter/Wrapper:
+    public class SmtpClientAdapter : ISmtpClient
+    {
+        private ISmtpClientAdaptee client;
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+        public int Timeout { get; set; }
+
+        public bool UseDefaultCredentials { get; set; }
+        public bool EnableSsl { get; set; }
+
+        public SmtpClientAdapter(ISmtpClientAdaptee client)
+        {
+            this.client = client;
+        }
+
+        public SmtpClientAdapter(ISmtpClientAdaptee client, string host) : this(client)
+        {
+            this.Host = host;
+        }
+
+        public SmtpClientAdapter(ISmtpClientAdaptee client, string host, int port) : this(client, host)
+        {
+            this.Port = port;
+        }
+
+        public void Send(MailMessage message)
+        {
+            client.Send(message);
+        }
+
+        public void Send(string from, string to, string subject, string body)
+        {
+            client.Send(from, to, subject, body);
+        }
+    }
+}
